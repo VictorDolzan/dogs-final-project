3f857a23554e75a07166e6dfe2239b9678bc1e03
Victor Dolzan <victorddf5@gmail.com>
2023-12-27 14:46:51 -0300

    Feito comportamento inicial do site

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,27 @@
 import React from "react";
+import './App.css';
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import Header from "./Components/Header/Header.jsx";
+import Footer from "./Components/Footer/Footer.jsx";
+import Home from "./Components/Home/Home.jsx";
+import Login from "./Components/Login/Login.jsx";
 
 function App() {
 
-  return (
-    <>
-     <div>App React</div>
-    </>
-  )
+    return (
+        <>
+            <div>
+                <BrowserRouter>
+                    <Header/>
+                    <Routes>
+                        <Route path="/" element={<Home />}/>
+                        <Route path="/login" element={<Login />}/>
+                    </Routes>
+                    <Footer/>
+                </BrowserRouter>
+            </div>
+        </>
+    )
 }
 
 export default App
