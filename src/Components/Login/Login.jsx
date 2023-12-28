b3da00b23fd0335f661008db1e260565379658e7
Victor Dolzan <victorddf5@gmail.com>
2023-12-28 10:29:32 -0300

    Feita parte inicial da tela de Login.jsx

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
 import styles from './Login.module.css';
+import {Route, Routes} from "react-router-dom";
+import LoginForm from "./Tabs/LoginForm/LoginForm.jsx";
+import LoginCreate from "./Tabs/LoginCreate/LoginCreate.jsx";
+import LoginPasswordLost from "./Tabs/LoginPasswordLost/LoginPasswordLost.jsx";
+import LoginPasswordReset from "./Tabs/LoginPasswordReset/LoginPasswordReset.jsx";
 
 const Login = () => {
     return (
         <div>
-            Login
+            <Routes>
+                <Route path="/" element={<LoginForm />}/>
+                <Route path="criar" element={<LoginCreate />}/>
+                <Route path="perdeu" element={<LoginPasswordLost />}/>
+                <Route path="resetar" element={<LoginPasswordReset />} />
+            </Routes>
         </div>
     );
 };
---
+++ src/Components/Login/Tabs/LoginCreate/LoginCreate.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const LoginCreate = () => {
+    return (
+        <div>
+            Esse é o loginCreate
+        </div>
+    );
+};
+
+export default LoginCreate;
