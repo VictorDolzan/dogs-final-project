234afdf56252ee978def5c0b72d1280d98f57926
Victor Dolzan <victorddf5@gmail.com>
2024-01-02 21:55:51 -0300

    Melhorado o layout de LoginForm.jsx

--- src/Components/Login/Tabs/LoginForm/LoginForm.jsx
+++ src/Components/Login/Tabs/LoginForm/LoginForm.jsx
@@ -4,11 +4,14 @@ import Input from "../../../Forms/Input/Input.jsx";
 import Button from "../../../Forms/Button/Button.jsx";
 import useForm from "../../../../Hooks/useForm.jsx";
 import {UserContext} from "../../../../Context/UserContext.jsx";
+import Error from "../../../Helper/Error/Error.jsx";
+import styles from './LoginForm.module.css';
+import stylesBtn from '../../../Forms/Button/Button.module.css';
 
 const LoginForm = () => {
     const username = useForm();
     const password = useForm();
-    const { userLogin, error, loading } = useContext(UserContext);
+    const {userLogin, error, loading} = useContext(UserContext);
 
     async function handleSubmit(event) {
         event.preventDefault();
@@ -19,9 +22,9 @@ const LoginForm = () => {
     }
 
     return (
-        <section>
-            <h1>Login</h1>
-            <form action="" onSubmit={handleSubmit}>
+        <section className="animeLeft">
+            <h1 className="title">Login</h1>
+            <form onSubmit={handleSubmit} className={styles.form}>
                 <Input label="Usuário" type="text" name="username" {...username} />
                 <Input label="Senha" type="password" name="password" {...password} />
                 {loading ? (
@@ -29,11 +32,14 @@ const LoginForm = () => {
                 ) : (
                     <Button>Entrar</Button>
                 )}
-                {error && (
-                    <p>{error}</p>
-                )}
+                <Error error={error}/>
             </form>
-            <Link to="/login/criar">Cadastro</Link>
+            <Link className={styles.perdeu} to="/login/perdeu">Perdeu a senha?</Link>
+            <div className={styles.cadastro}>
+                <h2 className={styles.subtitle}>Cadastre-se</h2>
+                <p>Ainda não possui conta? Cadastre-se no site.</p>
+                <Link className={stylesBtn.button} to="/login/criar">Cadastro</Link>
+            </div>
         </section>
     );
 };
