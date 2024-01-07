a59856ab46dc1a967a083c9f25bd0e9927235e7a
Victor Dolzan <victorddf5@gmail.com>
2024-01-07 19:31:43 -0300

    Feito a parte inicial do Feed.jsx

--- src/Components/Feed/Feed.jsx
+++ src/Components/Feed/Feed.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import FeedModal from "./nav/FeedModal/FeedModal.jsx";
+import FeedPhotos from "./nav/FeedPhotos/FeedPhotos.jsx";
 
 const Feed = () => {
     return (
         <div>
-            Feed
+            <FeedModal />
+            <FeedPhotos />
         </div>
     );
 };
