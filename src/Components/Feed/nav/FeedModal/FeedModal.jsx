4f9a31465aeaa3bbf0427acce82bf1548dc51c0b
Victor Dolzan <victorddf5@gmail.com>
2024-01-07 21:15:57 -0300

    Feito a parte inicial do FeedModal.jsx

--- src/Components/Feed/nav/FeedModal/FeedModal.jsx
+++ src/Components/Feed/nav/FeedModal/FeedModal.jsx
@@ -1,9 +1,35 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import styles from './FeedModal.module.css';
+import useFetch from "../../../../Hooks/useFetch.jsx";
+import {PHOTO_GET} from "../../../../Api.jsx";
+import Error from "../../../Helper/Error/Error.jsx";
+import Loading from "../../../Helper/Loading/Loading.jsx";
+import PhotoContent from "../../../Photo/PhotoContent/PhotoContent.jsx";
 
-const FeedModal = () => {
+const FeedModal = ({photo, setModalPhoto}) => {
+    const {data, error, loading, request} = useFetch();
+
+    useEffect(() => {
+        const {url, options} = PHOTO_GET(photo.id);
+        const {response, json} = request(url, options);
+
+    }, [photo, request]);
+
+    function handleOutsideClick(event){
+        if (event.target === event.currentTarget) setModalPhoto(null);
+    }
+
     return (
-        <div>
-            
+        <div className={styles.modal} onClick={handleOutsideClick}>
+            {error && (
+                <Error error={error} />
+            )}
+            {loading && (
+                <Loading />
+            )}
+            {data && (
+                <PhotoContent data={data}/>
+            )}
         </div>
     );
 };
