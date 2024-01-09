c276f35a12913a6999434448c9ea295db5ea222e
Victor Dolzan <victorddf5@gmail.com>
2024-01-09 19:57:56 -0300

    Feito o comportamento dos comentários

--- src/Components/Photo/PhotoComments/PhotoComments.jsx
+++ src/Components/Photo/PhotoComments/PhotoComments.jsx
@@ -1,15 +1,20 @@
-import React, {useContext, useState} from 'react';
+import React, {useContext, useEffect, useRef, useState} from 'react';
 import {UserContext} from "../../../Context/UserContext.jsx";
 import PhotoCommentsForm from "../PhotoCommentsForm/PhotoCommentsForm.jsx";
 import styles from './PhotoComments.module.css'
 
 const PhotoComments = ({id, comentarios}) => {
-    const [comments, setComments] = useState(() => comentarios)
+    const [comments, setComments] = useState(() => comentarios);
+    const commentsSection = useRef(null);
     const {login} = useContext(UserContext);
+
+    useEffect(() => {
+        commentsSection.current.scrollTop = commentsSection.current.scrollHeight;
+    }, [comments]);
 
     return (
         <>
-            <ul className={styles.comments}>
+            <ul ref={commentsSection} className={styles.comments}>
                 {comments && (comments.map(comment =>
                     <li key={comment.comment_ID}>
                         <b>{comment.comment_author}: </b>
