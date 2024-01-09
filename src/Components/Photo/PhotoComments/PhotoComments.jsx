a6979b4735afc29fe0f5b10be1b1c30b2f052ab6
Victor Dolzan <victorddf5@gmail.com>
2024-01-09 18:24:52 -0300

    Feito a parte de postagem de comentários e reatividade em fazer aparecer os comentários

--- src/Components/Photo/PhotoComments/PhotoComments.jsx
+++ src/Components/Photo/PhotoComments/PhotoComments.jsx
@@ -1,10 +1,26 @@
-import React from 'react';
+import React, {useContext, useState} from 'react';
+import {UserContext} from "../../../Context/UserContext.jsx";
+import PhotoCommentsForm from "../PhotoCommentsForm/PhotoCommentsForm.jsx";
+import styles from './PhotoComments.module.css'
 
-const PhotoComments = ({id, comments}) => {
+const PhotoComments = ({id, comentarios}) => {
+    const [comments, setComments] = useState(() => comentarios)
+    const {login} = useContext(UserContext);
+
     return (
-        <div>
-            Comentário
-        </div>
+        <>
+            <ul className={styles.comments}>
+                {comments && (comments.map(comment =>
+                    <li key={comment.comment_ID}>
+                        <b>{comment.comment_author}: </b>
+                        <span>{comment.comment_content}</span>
+                    </li>
+                ))}
+            </ul>
+            {login && (
+                <PhotoCommentsForm id={id} setComments={setComments}/>
+            )}
+        </>
     );
 };
 
