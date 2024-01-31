ea0e96787a0b35267026a0be1f28cd9cbfc8a872
Victor Dolzan <victorddf5@gmail.com>
2024-01-31 14:53:38 -0300

    📜Implementation of Redux in Modal behaviour

--- src/Components/Feed/nav/FeedPhotos/Tabs/FeedPhotosItem/FeedPhotosItem.jsx
+++ src/Components/Feed/nav/FeedPhotos/Tabs/FeedPhotosItem/FeedPhotosItem.jsx
@@ -1,10 +1,16 @@
-import React, {useEffect} from 'react';
+import React from 'react';
 import styles from './FeedPhotosItem.module.css';
 import Image from "../../../../../Helper/Image/Image.jsx";
+import {useDispatch} from "react-redux";
+import {openModal} from "../../../../../../store/actions/ui.jsx";
+import {fetchPhoto} from "../../../../../../store/modules/photo/action.jsx";
 
-const FeedPhotosItem = ({photo, chave, setModalPhoto}) => {
+const FeedPhotosItem = ({photo, chave}) => {
+    const dispatch = useDispatch();
+
     function handleClick() {
-        setModalPhoto(photo);
+        dispatch(openModal());
+        dispatch(fetchPhoto(photo.id));
     }
 
     return (
